3c8f702d2bf84eae18a985b27128e6d2d2b9ea72
VCarlosJP <carlosjp.villanueva@gmail.com>
2021-03-10 14:35:04 +0100

    Form Handler logic added

--- src/pages/quote.js
+++ src/pages/quote.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { graphql } from "gatsby";
 
 import Header from "../components/Header";
@@ -6,10 +6,33 @@ import Footer from "../components/Footer";
 import ContactInfo from "../components/contactInfo";
 
 import styles from "../styles/quote.module.css";
+import { postQuote } from "../services/postQuote.js";
 
 const Quote = ({ data }) => {
   const facebookLogo = data.allFile.edges[0].node.childImageSharp.fixed;
   const instagramLogo = data.allFile.edges[1].node.childImageSharp.fixed;
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [adressLine, setAddressLine] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [message, setMessage] = useState("");
+
+  const dataFromForm = {
+    name: name,
+    email: email,
+    addressLine: adressLine,
+    city: city,
+    state: state,
+    message: message,
+  };
+
+  const handleSubmit = (evt) => {
+    evt.preventDefault();
+    postQuote(dataFromForm);
+  };
+
   return (
     <div>
       <Header minified />
@@ -25,47 +48,85 @@ const Quote = ({ data }) => {
           </p>
           <div className={styles.ContactInfoContainer}>
             <ContactInfo contactIcon={facebookLogo}>
-              <a href="">1-456-254-7410</a>
+              <a href="tel:+1 913-230-4605">1-456-254-7410</a>
             </ContactInfo>
             <ContactInfo contactIcon={facebookLogo}>
-              <a href="">info@allaboutwood.com</a>
+              <a href="mailto:jonnymn_12@hotmail.com">info@allaboutwood.com</a>
             </ContactInfo>
             <ContactInfo contactIcon={facebookLogo}>
-              <a href="">All About Wood Services KC</a>
+              <a
+                href="https://www.facebook.com/All-about-Wood-Service-KC-102386658266078"
+                target="_blank"
+              >
+                All About Wood Services KC
+              </a>
             </ContactInfo>
           </div>
         </div>
         <div className={styles.containerColumnRight}>
-          <div className={styles.formContainer}>
+          <form onSubmit={handleSubmit} className={styles.formContainer}>
             <div className={styles.formElements}>
               <label>
                 Name<span className={styles.required}>*</span>
               </label>
-              <input type="text" />
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
 
               <label>
                 Email<span className={styles.required}>*</span>
               </label>
-              <input type="text" />
-              <label for="email">
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              <label>
                 Address<span className={styles.required}>*</span>
               </label>
-              <input type="email" placeholder="Address Line" />
+              <input
+                type="text"
+                value={adressLine}
+                onChange={(e) => setAddressLine(e.target.value)}
+                placeholder="Address Line"
+                required
+              />
 
               <div className={styles.formAdressContainer}>
-                <input type="email" placeholder="City" />
-                <input type="email" placeholder="State" />
+                <input
+                  type="text"
+                  value={city}
+                  onChange={(e) => setCity(e.target.value)}
+                  placeholder="City"
+                  required
+                />
+                <input
+                  type="text"
+                  value={state}
+                  onChange={(e) => setState(e.target.value)}
+                  placeholder="State"
+                  required
+                />
               </div>
 
               <label>
                 Message<span className={styles.required}>*</span>
               </label>
-              <textarea type="email" />
+              <textarea
+                type="text"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
               <button className={styles.submitButton} type="submit">
                 Send
               </button>
             </div>
-          </div>
+          </form>
         </div>
       </section>
       <Footer />
