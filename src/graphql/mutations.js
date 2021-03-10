459d0efdcfb5f868efea11fb65ce6dd9cc9bbbee
David Alvarado <d.alvarado@diatomea.tech>
2021-03-10 15:09:07 +0100

    Adding lambda function and api for handling the quote post request.

---
+++ src/graphql/mutations.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const createQuote = /* GraphQL */ `
+  mutation CreateQuote($input: CreateQuoteInput!) {
+    createQuote(input: $input) {
+      name
+      email
+      addressLine
+      addessCity
+      addressState
+      message
+      status
+    }
+  }
+`;
---
+++ src/graphql/queries.js
@@ -0,0 +1,16 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const listQuotes = /* GraphQL */ `
+  query ListQuotes {
+    listQuotes {
+      name
+      email
+      addressLine
+      addessCity
+      addressState
+      message
+      status
+    }
+  }
+`;
